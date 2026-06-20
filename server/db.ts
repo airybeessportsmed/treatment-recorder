1d1ded90e8573d951187abcb005fa3026cf2ffb6
airybeessportsmed <airybees.sportsmed.2019@gmail.com>
2026-06-20 11:22:54 +0900

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -22,7 +22,7 @@ export async function getDb() {
         },
       });
 
-      _db = drizzle(pool);
+      _db = drizzle(pool) as any;
     } catch (error) {
       console.warn("[Database] Failed to connect:", error);
       _db = null;
